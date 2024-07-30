e5f3d9cd288f74dcab09c3b7f01b1a6539cd7b4d
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-07-30 11:47:44 -0400

    added the v-card in the addMenuItem component in frontend

--- frontend/src/components/addMenuItem.vue
+++ frontend/src/components/addMenuItem.vue
@@ -1,11 +1,44 @@
 <template>
-  <v-container class="pa-3"> add menuItem </v-container>
+  <v-container>
+    <v-row justify="space-around">
+      <v-col cols="12">
+        <v-card
+          max-width="800"
+          varian="outlined"
+          elevation="0"
+          class="mx-auto form pa-10 border-md"
+        >
+          <h2 class="text-decoration-underline text-center" :hidden="xs">
+            Add A New MenuItem
+          </h2>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      xs: false as boolean,
+    };
+  },
+  mounted() {
+    this.checkWidth();
+  },
+  methods: {
+    checkWidth() {
+      if (window.innerWidth < 600) {
+        this.xs = true;
+      } else {
+        this.xs = false;
+      }
+    },
+  },
+});
 </script>
 
 <style></style>
