e52bbfab6563b8eab95859deb1f4fa9276a6c3de
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2024-08-16 06:45:40 -0400

    aligned the recipe and added two lines in the layout

--- frontend/src/components/singleMenuItem.vue
+++ frontend/src/components/singleMenuItem.vue
@@ -9,14 +9,18 @@
       <v-col cols="12" xs="12" sm="6" md="6" lg="6">
         <v-sheet>
           <img class="menuImage" :src="menuItem.photo" alt="" />
-          <div class="recipe">
-            <h2 class="text-decoration-underline font-weight-medium">
-              Recipe :
-            </h2>
-            <h3 class="font-weight-regular">{{ menuItem.recipe }}</h3>
-          </div>
         </v-sheet>
       </v-col>
+
+      <v-col cols="12" xs="12" sm="6" md="6" lg="6" xl="6">
+        <div class="recipe">
+          <h2 class="text-decoration-underline font-weight-medium">Recipe :</h2>
+          <h3 class="font-weight-regular">{{ menuItem.recipe }}</h3>
+        </div>
+      </v-col>
+
+      <div class="line first-line"></div>
+      <div class="line second-line"></div>
     </v-row>
   </v-container>
 </template>
@@ -29,6 +33,7 @@ export default defineComponent({
   data() {
     return {
       menuItem: {} as object,
+      ingredients: [] as object[],
     };
   },
   async mounted() {
@@ -37,7 +42,13 @@ export default defineComponent({
       .then((response) => {
         this.menuItem = response.data;
       });
-    console.log(this.menuItem);
+
+    await axios
+      .get(`http://localhost:5000/getMenuIngredient/${this.$route.params.id}`)
+      .then((response) => {
+        this.ingredients = response.data;
+      });
+    console.log(this.ingredients);
   },
 });
 </script>
@@ -75,5 +86,23 @@ export default defineComponent({
 
 .lower-text-line {
   margin-bottom: 20px;
+}
+
+.line {
+  width: 100%;
+  border: 0.5px solid black;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  margin: auto;
+  margin: 2px 0;
+}
+
+.first-line {
+  margin-top: 30px;
+}
+
+.second-line {
+  margin-bottom: 30px;
 }
 </style>
